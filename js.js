b67e5564bc580f994f45a119130924eec2633a01
Dygle <33068971+Dygle@users.noreply.github.com>
2019-04-24 19:34:57 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,73 @@
+
+// var a = +prompt("Введите число a");
+// var b = +prompt("Введите число b");
+
+// if (a > b) {
+// 	console.log("Большее число a " + a);
+// } else {
+// 	console.log("Большее число b " + b);
+// }
+
+
+
+// console.log("\nЗадание 2");
+
+// var a = prompt("Введите число a");
+// var b = prompt("Введите число b");
+
+// if (a * b > 0)
+// 	console.log("Знак у числа '+'");
+// else if (a == 0 || b == 0)
+// 	console.log("Умножение на 0")
+// else console.log("Знак у числа '-'");
+// console.log('\n');
+
+
+
+
+// console.log("\nЗадание 3");
+
+// var a = +prompt("Введите число a");
+// var b = +prompt("Введите число b");
+// var c = +prompt("Введите число c");
+
+// if (a > b && a > c) {
+// 	console.log("Первое число " + a);
+// 	if (b>c) {
+// 		console.log("\nВторое число " + b + "\nТретье число" + c);
+// 	}
+// 	else console.log("\nВторое число " + c + "\nТретье число" + b);
+// }
+// else if (b > c && b > a) {
+// 	console.log("Первое число " + b);
+// 	if (a>c) {
+// 		console.log("Второе число " + a + "\nТретье число" + c);
+// 	}
+// 	else console.log("Второе число " + c + "\nТретье число" + a);
+// }
+// else if (c > a && c > b) {
+// 	console.log("Первое число " + c);
+// 	if (a>b) {
+// 		console.log("Второе число " + a + "\nТретье число" + b);
+// 	}
+// 	else console.log("Второе число " + b + "\nТретье число" + a);
+// }
+
+
+
+
+// console.log("\nЗадание 4");
+// var a = +prompt("Введите число a");
+// var b = +prompt("Введите число b");
+
+// (a > b) ? console.log("Число 'a': " + a + " - больше числа b: " + b) : console.log("Число 'b': " + b + " - больше числа 'a': " + a);
+
+// console.log("\nЗадание 5");
+
+console.log("\nЗадание 7");
+
+var x = +prompt("");
+
+for (var i = 0; i < x; i++) {
+	if (i % 2 == 0) console.log("Число " + i + " четное.");
+}
