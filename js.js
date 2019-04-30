d2e8abdaa0733ded4f288c791449006a539378dc
Dygle <33068971+Dygle@users.noreply.github.com>
2019-04-30 19:32:41 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,27 @@
+// function task1(a,b,c) {
+// 	if (c ** 2 == a ** 2 + b **2) {
+// 		console.log("True");
+// 	}
+// 	else console.log("False");
+// }
+
+// console.log(task1(5,5,10));
+
+// function repeat (str, n = 5) {
+// 	let result = "";
+// 	for (let i = 0; i < n; i++ ) {
+// 		result += str;
+// 	}
+// 	return result;
+// }
+
+// console.log(repeat("asd "));
+
+function school(kids, parta) {
+	if (kids > parta && parta == kids/2) {
+		return "Парт достаточно.";
+	}
+	else if (parta <)
+}
+
+console.log(school(10,5));
